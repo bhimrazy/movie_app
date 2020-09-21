b6ee4f9e6597a0aa6f731ba26c424e00a5a447fa
bhimrazy <bhimrajyadav977@gmail.com>
2020-09-21 18:04:00 +0545

    genres section added

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/screens/components/categories.dart';
+import 'package:movie_app/screens/components/genres.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -7,6 +8,7 @@ class Body extends StatelessWidget {
     return Column(
       children: [
         CategoryList(),
+        Genres(),
       ],
     );
   }
---
+++ lib/screens/components/genres.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/constants.dart';
+import 'package:movie_app/screens/components/genre_card.dart';
+
+class Genres extends StatelessWidget {
+  List<String> genres = ["Action", "Crime", "Comedy", "Horror", "Animation"];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+      height: 36,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: genres.length,
+          itemBuilder: (context, index) => GenreCard(genre: genres[index])),
+    );
+  }
+}
